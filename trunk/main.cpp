0afb273eb12be48549e9d1a7b11effbfb697dd18
itrekkie <itrekkie@7d7cb4e2-b615-11dd-a828-c573283ffd12>
2008-11-29 05:58:32 +0000

    Update German translation and fix actual translation code to respect the user's wishes, not mine.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -6,15 +6,15 @@ int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
 
+    // Translate both Qt and the application
+    // These translations may move in the future to a seperate directory
+    QString locale = application.desktop()->locale().name();
     QTranslator qtTranslator;
-    qtTranslator.load("qt_de", QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    qtTranslator.load("qt_" + locale, QLibraryInfo::location(QLibraryInfo::TranslationsPath));
     application.installTranslator(&qtTranslator);
-
-    QTranslator translator;
-    QString locale = QLocale::system().name();
-
-    translator.load("QtBoggle_de");
-    application.installTranslator(&translator);
+    QTranslator appTranslator;
+    appTranslator.load("QtBoggle_" + locale);
+    application.installTranslator(&appTranslator);
 
     MainWindow window;
     window.show();
