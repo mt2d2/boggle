c1007d87ea2afa2e08a7524b5719175dd96be564
itrekkie <itrekkie@7d7cb4e2-b615-11dd-a828-c573283ffd12>
2008-12-22 07:49:21 +0000

    Updated german translation to ignore old rich text edit entry.

--- trunk/translations/de.ts
+++ trunk/translations/de.ts
@@ -4,49 +4,39 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="208"/>
+        <location filename="../sources/mainwindow.cpp" line="208"/>
         <source>Time remaining: %1</source>
-        <translation>Übrige Zeit: %1</translation>
+        <translation>Übrige Zeite: %1</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="121"/>
+        <location filename="../sources/mainwindow.cpp" line="121"/>
         <source>Stop</source>
         <translation>Aufhören</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="48"/>
+        <location filename="../sources/mainwindow.ui" line="48"/>
         <source>Press Start to begin the game...</source>
-        <translation>Drucken Sie Beginnen, um anzufangen...</translation>
+        <translation>﻿Drucken Sie Beginnen, um anzufangen...</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="71"/>
+        <location filename="../sources/mainwindow.ui" line="71"/>
         <source>Start</source>
         <translation>Beginnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="172"/>
+        <location filename="../sources/mainwindow.ui" line="418"/>
         <source>Enter words seperated by one space...</source>
-        <translation>Bitte geben Sie Wörter ein, die durch ein Leerzeichen abgetrennt werden...</translation>
+        <translation>﻿Bitte geben Sie Wörter ein, die durch ein Leerzeichen abgetrennt werden...</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="26"/>
+        <location filename="../sources/mainwindow.ui" line="26"/>
         <source>Boggle</source>
-        <translation>Boggle</translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="396"/>
+        <location filename="../sources/mainwindow.ui" line="396"/>
         <source>_</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="418"/>
-        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
-&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
-p, li { white-space: pre-wrap; }
-&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Lucida Grande&apos;; font-size:13pt; font-weight:400; font-style:normal;&quot;&gt;
-&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Enter words seperated by one space...&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;p, li { white-space: pre-wrap; }&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Lucida Grande&apos;; font-size:13pt; font-weight:400; font-style:normal;&quot;&gt;&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Bitte geben Sie Wörter ein, die durch ein Leerzeichen abgetrennt werden...
-&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation></translation>
     </message>
 </context>
 </TS>
