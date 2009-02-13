0e8fcc08bdbd1dbb26b33176d3c36a73df6fbf97
itrekkie <itrekkie@7d7cb4e2-b615-11dd-a828-c573283ffd12>
2009-02-13 06:50:26 +0000

    Updated translation to German: added message dialog translation.

--- translations/de.ts
+++ translations/de.ts
@@ -1,28 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1" language="de">
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../sources/mainwindow.cpp" line="199"/>
+        <location filename="../sources/mainwindow.cpp" line="113"/>
+        <source>Vald words: %1
+Words not found: %2</source>
+        <translation>Gültige Wörter:%1
+Wörter, die nicht gefunden wurden:%2</translation>
+    </message>
+    <message>
+        <location filename="../sources/mainwindow.cpp" line="130"/>
+        <location filename="../sources/mainwindow.cpp" line="188"/>
         <source>Time remaining: %1</source>
         <translation>Übrige Zeite: %1</translation>
     </message>
     <message>
-        <location filename="../sources/mainwindow.cpp" line="134"/>
+        <location filename="../sources/mainwindow.cpp" line="133"/>
         <source>Stop</source>
         <translation>Aufhören</translation>
     </message>
     <message>
+        <location filename="../sources/mainwindow.cpp" line="162"/>
         <location filename="../sources/mainwindow.ui" line="48"/>
         <source>Press Start to begin the game...</source>
         <translation>﻿Drucken Sie Beginnen, um anzufangen...</translation>
     </message>
     <message>
+        <location filename="../sources/mainwindow.cpp" line="165"/>
         <location filename="../sources/mainwindow.ui" line="71"/>
         <source>Start</source>
         <translation>Beginnen</translation>
     </message>
     <message>
+        <location filename="../sources/mainwindow.cpp" line="168"/>
         <location filename="../sources/mainwindow.ui" line="98"/>
         <source>Enter words seperated by one space...</source>
         <translation>﻿Bitte geben Sie Wörter ein, die durch ein Leerzeichen abgetrennt werden...</translation>
